735c8750a0aed97554c01a84e7188f4a86ce4457
angisdev <anggasaputro.w@gmail.com>
2024-09-10 19:29:19 +0700

    feat: fixing styling

---
+++ app/about-us/arthawena-at-glance/images.tsx
@@ -0,0 +1,32 @@
+//Image Collectionss
+import asset1 from "@/app/assets/arthawena-at-glance/Asset 1.png"
+import asset2 from "@/app/assets/arthawena-at-glance/Asset 2.png"
+import asset3 from "@/app/assets/arthawena-at-glance/Asset 3.png"
+import asset4 from "@/app/assets/arthawena-at-glance/Asset 4.png"
+import asset5 from "@/app/assets/arthawena-at-glance/Asset 5.png"
+import asset6 from "@/app/assets/arthawena-at-glance/Asset 6.png"
+import asset7 from "@/app/assets/arthawena-at-glance/Asset 7.png"
+import asset8 from "@/app/assets/arthawena-at-glance/Asset 8.png"
+import asset9 from "@/app/assets/arthawena-at-glance/Asset 9.png"
+import asset10 from "@/app/assets/arthawena-at-glance/Asset 10.png"
+import asset11 from "@/app/assets/arthawena-at-glance/Asset 11.png"
+import asset12 from "@/app/assets/arthawena-at-glance/Asset 12.png"
+import asset13 from "@/app/assets/arthawena-at-glance/Asset 13.png"
+import asset14 from "@/app/assets/arthawena-at-glance/Asset 14.png"
+
+export const Images = {
+    asset1: asset1,
+    asset2: asset2,
+    asset3: asset3,
+    asset4: asset4,
+    asset5: asset5,
+    asset6: asset6,
+    asset7: asset7,
+    asset8: asset8,
+    asset9: asset9,
+    asset10: asset10,
+    asset11: asset11,
+    asset12: asset12,
+    asset13: asset13,
+    asset14: asset14,
+}
