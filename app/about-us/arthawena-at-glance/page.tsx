ae5e9e46fe149ed1b4b2145d2b3b360f6048c0e7
angisdev <anggasaputro.w@gmail.com>
2024-09-16 21:16:39 +0700

    fix: Images resolution and Sanity Query for Arthawena at glance page

--- app/about-us/arthawena-at-glance/page.tsx
+++ app/about-us/arthawena-at-glance/page.tsx
@@ -1,11 +1,14 @@
+// Import Data //
+import { client } from "@/sanity/lib/client";
+import { PortableText } from "@portabletext/react";
+import { arthawenaAtGlanceQuery } from "@/sanity/services/arthawena-at-glance-query"
+import { ArthawenaAtGlanceType } from "@/types/arthawena-at-glance-type"
+
 // Import Components //
 import DynamicBreadcrumb from "@/components/molecules/dynamic-bradcrumb";
 import Image from "next/image";
-import { client } from "@/sanity/lib/client";
-import { PortableText } from "next-sanity";
 import portableTextComponents from "@/components/atoms/portable-text";
-import { arthawenaAtGlanceQuery } from "@/sanity/services/arthawena-at-glance-query"
-import { ArthawenaAtGlanceType } from "@/types/arthawena-at-glance-type"
+
 
 export default async function ArthawenaAtGlance() {
   const arthawenaAtGlanceData: ArthawenaAtGlanceType[] = await client.fetch(arthawenaAtGlanceQuery);
@@ -22,7 +25,7 @@ export default async function ArthawenaAtGlance() {
       {/* <-- === Breadcrumb End === --> */}
 
       {/* <-- === The content must be placed right here === --> */}
-      <main className="px-content-padding-sm pt-content-padding-top-sm lg:pt-content-padding-top-lg 2xl:pt-content-padding-top-2xl lg:px-content-padding-lg 2xl:px-content-padding-2xl">
+      <main className="px-content-padding-sm lg:px-content-padding-lg 2xl:px-content-padding-2xl pt-content-padding-top-sm lg:pt-content-padding-top-lg 2xl:pt-content-padding-top-2xl mb-40 lg:mb-48">
 
       {/* section at glance */}
       <section className="flex flex-col gap-8 lg:items-center justify-center">
@@ -33,29 +36,40 @@ export default async function ArthawenaAtGlance() {
         <div className="lg:w-full lg:h-1 lg:bg-primary"></div>
       </section>
 
-      <section className="flex flex-col mt-20 gap-20 lg:items-center lg:justify-center lg:w-auto">
+      <section className="flex flex-col mt-20 gap-16 lg:items-center lg:justify-center lg:w-auto">
       {arthawenaAtGlanceData.map((arthawenaAtGlance) => (
-      <div key={`arthawena-at-glance-${arthawenaAtGlance.title}`} className="text-charcoal flex flex-col lg:justify-center lg:items-center lg:w-[59%]">
-        <Image 
-        src={arthawenaAtGlance.imageUrl}
-        alt={arthawenaAtGlance.title}
-        width={0}
-        height={0}
-        sizes="100vw"
-        priority={true}
-        quality={100}
-        className="object-cover object-center w-auto h-16"
-        ></Image>
-        <h1 className="text-2xl font-semibold my-7">{arthawenaAtGlance.title}</h1>
-        <div className="font-light text-21 lg:text-center">
+      <div key={`arthawena-at-glance-${arthawenaAtGlance.title}`} className="text-charcoal flex flex-col lg:justify-center lg:items-center lg:w-[70%]">
+        <div className="w-fit block object-center">
+                <Image
+                  src={arthawenaAtGlance.imageUrl}
+                  alt={arthawenaAtGlance.title}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  priority={true}
+                  quality={100}
+                  className="w-auto h-20 lg:[70px]"
+                ></Image>
+              </div>
+              <h1 className="text-2xl font-semibold my-7 lg:mt-12">
+                {arthawenaAtGlance.title}
+              </h1>
+
+        {/* Description Section */}
+        {arthawenaAtGlance.description &&
+          Array.isArray(arthawenaAtGlance.description) && (
+        <div className="text-opacity-80 text-base leading-relaxed lg:text-center whitespace-pre-line">
           <PortableText 
           value={arthawenaAtGlance.description}
           components={portableTextComponents}
           />
         </div>
-        <div className="lg:text-center font-light text-21 mt-6">
-        <p className="font-medium">{arthawenaAtGlance.employee}</p>
-        <p>{arthawenaAtGlance.location}</p>
+        )}
+
+        {/* Employee and Location Section */}
+        <div>
+        <p className="text-opacity-80 text-base font-medium mt-6 lg:text-center">{arthawenaAtGlance.employee}</p>
+        <p className="text-opacity-80 text-base font-medium lg:text-center">{arthawenaAtGlance.location}</p>
         </div>
       </div>
       ))
