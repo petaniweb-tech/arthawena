16714ec07de3cec4d539f43e057313b6465e744b
ilyas-muhammad <ell@petaniweb.com>
2024-09-24 12:43:00 +0700

    fix: make video-player component dumb

--- components/atoms/video-player.tsx
+++ components/atoms/video-player.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import React from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
@@ -8,112 +8,47 @@ import pause from "@/assets/icons/pause.svg";
 
 interface VideoPlayerProps {
   videoSrc: string;
-  videoRef?: React.RefObject<HTMLVideoElement>;
-  isVideoPlay?: boolean;
-  muted?: boolean;
-  onManualPause?: () => void;
-  onVideoEnd?: () => void;
+  videoRef: React.RefObject<HTMLVideoElement>;
+  isPlaying: boolean;
+  isMuted: boolean;
+  currentTime: number;
+  onVideoClick: () => void;
+  onVideoEnded: () => void;
 }
 
 export default function VideoPlayer({
   videoSrc,
   videoRef,
-  isVideoPlay = false,
-  muted = true,
-  onManualPause,
-  onVideoEnd,
+  isPlaying,
+  isMuted,
+  currentTime,
+  onVideoClick,
+  onVideoEnded,
 }: VideoPlayerProps) {
-  const [isPlaying, setIsPlaying] = useState(isVideoPlay);
-  const [isMuted, setIsMuted] = useState(muted);
-  const [fadeOut, setFadeOut] = useState(false);
-  const [hasStarted, setHasStarted] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
-  const internalVideoRef = useRef<HTMLVideoElement>(null);
-  const finalVideoRef = videoRef || internalVideoRef;
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
-
-  const handleVideoClick = () => {
-    if (finalVideoRef.current) {
-      if (isPlaying) {
-        finalVideoRef.current.pause();
-        clearTimeout(timerRef.current as NodeJS.Timeout);
-        setFadeOut(false);
-        setIsPaused(true);
-
-        if (onManualPause) {
-          onManualPause();
-        }
-      } else {
-        finalVideoRef.current.currentTime = 1;
-        // If it's the first play, start from second 1
-        if (!hasStarted) {
-          finalVideoRef.current.currentTime = 1;
-          setHasStarted(true); // Mark the video as having been started
-        } else if (isPaused) {
-          // If resuming after pause, ensure video plays till the end
-          finalVideoRef.current.onended = () => {
-            if (onVideoEnd) {
-              onVideoEnd(); // Call the onVideoEnd callback when video ends
-            }
-          };
-        }
-        finalVideoRef.current.play();
-        finalVideoRef.current.muted = false;
-        setIsMuted(false);
-        setIsPaused(false);
-
-        clearTimeout(timerRef.current as NodeJS.Timeout);
-        timerRef.current = setTimeout(() => {
-          setFadeOut(true);
-        }, 1500);
-      }
-
-      setIsPlaying(!isPlaying);
-    }
-  };
-
-  useEffect(() => {
-    if (finalVideoRef.current) {
-      finalVideoRef.current.muted = isMuted;
-
-      // Attach onended event to move to the next slide when video ends
-      finalVideoRef.current.onended = () => {
-        if (onVideoEnd) {
-          onVideoEnd(); // Call the onVideoEnd callback when video ends
-        }
-      };
-    }
-  }, [isMuted, finalVideoRef, onVideoEnd]);
-
-  useEffect(() => {
-    setIsPlaying(isVideoPlay);
-    setFadeOut(false);
-  }, [isVideoPlay]);
-
   return (
     <main
-      onClick={handleVideoClick}
+      onClick={onVideoClick}
       className="flex items-center w-full h-screen justify-center origin-center relative"
     >
       <video
-        ref={finalVideoRef}
-        autoPlay={false}
+        ref={videoRef}
         muted={isMuted}
         loop={false}
         controls={false}
         preload="metadata"
         playsInline
         className="object-cover object-center h-full w-full"
+        onEnded={onVideoEnded}
       >
         <source src={videoSrc} type="video/webm" />
       </video>
 
       <div className="absolute inset-0 flex items-center justify-center">
         <button
-          onClick={handleVideoClick}
+          onClick={onVideoClick}
           className={cn(
             "flex items-center justify-center p-9 rounded-full bg-white bg-opacity-10 transition-opacity duration-500",
-            fadeOut ? "opacity-0" : "opacity-100"
+            isPlaying ? "opacity-0" : "opacity-100"
           )}
         >
           <Image
