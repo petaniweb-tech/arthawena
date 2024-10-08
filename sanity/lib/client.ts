318b6d0e98d16b40ff898bf7b2774b136679ed48
ilyas-muhammad <ell@petaniweb.com>
2024-10-08 13:46:12 +0700

    fix: not use cdn

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+});
